807ffb6f6e5834a9cded6164d0b2187d338da0bb
Benn <bennpm99@gmail.com>
2021-01-24 22:04:19 -0500

    feat: about us page beginning

--- src/components/hamburger.tsx
+++ src/components/hamburger.tsx
@@ -1,18 +1,20 @@
 import React, { FC } from 'react'
-import styled, { css } from 'styled-components'
+import styled, { css, DefaultTheme } from 'styled-components'
 
 interface HamburgerProps {
   isOpen: boolean
   onClick: () => void
+  white?: keyof DefaultTheme['colors']
 }
 
 interface BunsProps {
   isOpen: boolean
+  whiteLine?: keyof DefaultTheme['colors']
 }
 
-export const Hamburger: FC<HamburgerProps> = ({ isOpen, onClick }) => (
-  <Buns onClick={onClick} isOpen={isOpen}>
-    <Line position="top" isOpen={isOpen} />
+export const Hamburger: FC<HamburgerProps> = ({ isOpen, onClick, white }) => (
+  <Buns onClick={onClick} isOpen={isOpen} whiteLine={white}>
+    <Line position="top" isOpen={isOpen} whiteLine={white} />
     <Line position="bottom" isOpen={isOpen} />
   </Buns>
 )
@@ -39,17 +41,19 @@ const Buns = styled.div<BunsProps>`
 interface LineProps {
   position: 'top' | 'bottom'
   isOpen: boolean
+  whiteLine?: keyof DefaultTheme['colors']
 }
 const Line = styled.div<LineProps>`
+  background: white;
   position: absolute;
   display: block;
   width: 100%;
   height: 0.3125rem;
-  background: white;
+
   border-radius: 0.125rem;
   transition: 0.2s;
 
-  ${({ position, isOpen }) => {
+  ${({ position, isOpen, whiteLine, theme }) => {
     switch (position) {
       case 'top':
         return isOpen
@@ -59,6 +63,7 @@ const Line = styled.div<LineProps>`
             `
           : css`
               top: 0;
+              background: ${whiteLine && theme.colors[whiteLine]}};
             `
       case 'bottom':
         return css`
@@ -73,6 +78,7 @@ const Line = styled.div<LineProps>`
             : css`
                 bottom: 0;
                 right: 0;
+                background: ${!whiteLine && theme.colors['gray']};
               `}
         `
     }
